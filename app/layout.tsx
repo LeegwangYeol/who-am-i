2bf1049b1ee1d8ef9c9f67d788a876d0d52405a4
Zeronimo <bpscokr003@naver.com>
2025-03-07 23:19:01 +0900

    save

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import  Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://static.llami.net/widget-v1.css"
+        />
+      <Script type="module" id="llami-chat-widget" strategy="lazyOnload">
+          {`
+            import { initialize, run } from "https://static.llami.net/widget-v1.js";
+            run("9afddf76-2d21-422c-a4fc-a369fcf21d09");
+          `}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
