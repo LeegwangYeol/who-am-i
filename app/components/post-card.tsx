fdcb33d5f8216b5476ec5ce7ab4e609a235a6218
Zeronimo <bpscokr003@naver.com>
2025-03-09 23:03:41 +0900

    save

--- app/components/post-card.tsx
+++ app/components/post-card.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react"
+// import { useState } from "react"
 import Image from "next/image"
 import { Heart, MessageCircle, Bookmark, Share2 } from "lucide-react"
 
