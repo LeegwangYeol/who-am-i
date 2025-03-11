501e1c93310d715fa522dfd1ddc44b3d15511baa
Zeronimo <bpscokr003@naver.com>
2025-03-11 13:27:02 +0900

    save

--- app/components/post-card.tsx
+++ app/components/post-card.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 // import { useState } from "react"
-import Image from "next/image"
+// import Image from "next/image"
 import { Heart, MessageCircle, Bookmark, Share2 } from "lucide-react"
 
 // 강조 텍스트를 처리하는 컴포넌트
@@ -80,12 +80,9 @@ const defaultLikes = {
 };
 
 export default function PostCard({
-  profileImage,
   userName,
   category,
   categoryValue,
-  postImage,
-  postImageAlt,
   likeKey,
   content,
   highlights,
