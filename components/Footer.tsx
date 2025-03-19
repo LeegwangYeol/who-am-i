79c29e27ace83767fee65a16fb183554c87fc7b1
Zeronimo <bpscokr003@naver.com>
2025-03-20 02:06:30 +0900

    Svae

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -19,7 +19,6 @@ export default function Footer() {
                 fill={isDarkTheme ? "white" : "black"}
               >
                 <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-                <path d="M18 프론트엔드 웹 개발 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
               </svg>
               <a href="mailto:bpscokr003@naver.com" className="text-blue-500 hover:underline">
                 <span className={`${isDarkTheme ? 'text-white' : 'text-black'} overflow-hidden whitespace-nowrap hover:overflow-visible hover:whitespace-normal`}>
